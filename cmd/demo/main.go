5f52e6122a56c733eb02f23ed87beb4636764c60
hinst <hinstance@yandex.ru>
2023-06-11 17:21:23 +0200

    Draw simple menu

--- cmd/demo/main.go
+++ cmd/demo/main.go
@@ -2,17 +2,22 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 	ebiten_menu "github.com/hinst/ebiten-menu"
 )
 
 type Game struct {
-	menu ebiten_menu.MenuUserInterface
+	UpdateTime      time.Time
+	JustPressedKeys []ebiten.Key
+	Menu            ebiten_menu.MenuUserInterface
 }
 
 func (me *Game) Initialize() {
-	me.menu = ebiten_menu.MenuUserInterface{
+	me.UpdateTime = time.Now()
+	me.Menu = ebiten_menu.MenuUserInterface{
 		Items: []ebiten_menu.MenuUserInterfaceItem{
 			{
 				Title: "New Game",
@@ -31,11 +36,24 @@ func (me *Game) Initialize() {
 }
 
 func (me *Game) Update() error {
+	me.JustPressedKeys = inpututil.AppendJustPressedKeys(me.JustPressedKeys)
 	return nil
 }
 
 func (me *Game) Draw(screen *ebiten.Image) {
-	me.menu.Draw(screen)
+	var updateTime = time.Now()
+	me.update(updateTime.Sub(me.UpdateTime).Seconds())
+	me.UpdateTime = updateTime
+	me.JustPressedKeys = me.JustPressedKeys[:0]
+	me.draw(screen)
+}
+
+func (me *Game) update(deltaTime float64) {
+	me.Menu.Update(deltaTime, me.JustPressedKeys)
+}
+
+func (me *Game) draw(screen *ebiten.Image) {
+	me.Menu.Draw(screen)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -45,7 +63,9 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 func main() {
 	ebiten.SetWindowSize(640, 480)
 	ebiten.SetWindowTitle("Hello, World!")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	var game = &Game{}
+	game.Initialize()
+	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
 	}
 }
